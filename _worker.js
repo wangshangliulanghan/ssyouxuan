4c1e61d55ff6fbcddd596db56fcf11ff6570c0c0
shiyikeji <138283176+shiyikeji@users.noreply.github.com>
2026-03-07 11:19:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -179,7 +179,8 @@ function generateNodesFromList(list, user, workerDomain, disableNonTLS, customPa
         const nodeNameBase = item.colo && item.colo.trim() ? `${baseName}-${item.colo.trim()}` : baseName;
         const safeIP = item.ip.includes(':') ? `[${item.ip}]` : item.ip;
         
-        const portsToGenerate = item.port ? [item.port] : disableNonTLS ? [443] : [443, 80];
+// 无论原始数据提供什么端口，只要是 TLS 模式就强制锁定 443
+const portsToGenerate = disableNonTLS ? [443] : (item.port ? [item.port] : [443, 80]);
 
         for (const port of portsToGenerate) {
             const tls = port !== 80 && port !== 8080;
